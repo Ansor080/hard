a265d1be0d449540d2ab3691d0950ba85b70877a
Ansor080 <1000cpsbw@gmail.com>
2024-05-22 19:53:37 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".btnn");
+const btn2 = document.querySelector(".btnnn");
+
+btn.onclick = (btn) => {
+    btn.classList.toggle("lupa");
+};
+
+btn2.onclick = (btn2) => {
+    btn2.classList.toggle("sumka");
+};
